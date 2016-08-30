4976cf886dd62a8e5dd82a76501025dfcd9084bf
Pavlo Sumkin <psumkin@mirantis.com>
2016-08-30 18:22:53 +0300

    Work out custom provider
    https://www.hashicorp.com/blog/terraform-custom-providers.html

---
+++ provider.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+// Provider returns a terraform.ResourceProvider
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{
+			"dummy_server": resourceServer(),
+		},
+	}
+}
---
+++ test.tf
@@ -0,0 +1,3 @@
+resource "dummy_server" "foo" {
+    address = "127.0.0.1"
+}
